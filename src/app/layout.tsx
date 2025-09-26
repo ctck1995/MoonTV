315eb1f74dc26a089e27534ebb45e9f44ac9d7b7
cao kun <31645728+ctck1995@users.noreply.github.com>
2025-09-26 13:43:54 +0800

    feat: set defaultTheme to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,7 +109,7 @@ export default async function RootLayout({
       >
         <ThemeProvider
           attribute='class'
-          defaultTheme='system'
+          defaultTheme='dark'
           enableSystem
           disableTransitionOnChange
         >
